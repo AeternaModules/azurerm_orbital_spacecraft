514062ae20c004d8a4d993c743ae8c98410bc8f1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:32 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "orbital_spacecrafts" {
-  description = "All orbital_spacecraft resources"
-  value       = azurerm_orbital_spacecraft.orbital_spacecrafts
-}
 output "orbital_spacecrafts_links" {
-  description = "List of links values across all orbital_spacecrafts"
-  value       = [for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : v.links]
+  description = "Map of links values across all orbital_spacecrafts, keyed the same as var.orbital_spacecrafts"
+  value       = { for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : k => v.links }
 }
 output "orbital_spacecrafts_location" {
-  description = "List of location values across all orbital_spacecrafts"
-  value       = [for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : v.location]
+  description = "Map of location values across all orbital_spacecrafts, keyed the same as var.orbital_spacecrafts"
+  value       = { for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : k => v.location }
 }
 output "orbital_spacecrafts_name" {
-  description = "List of name values across all orbital_spacecrafts"
-  value       = [for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : v.name]
+  description = "Map of name values across all orbital_spacecrafts, keyed the same as var.orbital_spacecrafts"
+  value       = { for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : k => v.name }
 }
 output "orbital_spacecrafts_norad_id" {
-  description = "List of norad_id values across all orbital_spacecrafts"
-  value       = [for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : v.norad_id]
+  description = "Map of norad_id values across all orbital_spacecrafts, keyed the same as var.orbital_spacecrafts"
+  value       = { for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : k => v.norad_id }
 }
 output "orbital_spacecrafts_resource_group_name" {
-  description = "List of resource_group_name values across all orbital_spacecrafts"
-  value       = [for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : v.resource_group_name]
+  description = "Map of resource_group_name values across all orbital_spacecrafts, keyed the same as var.orbital_spacecrafts"
+  value       = { for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : k => v.resource_group_name }
 }
 output "orbital_spacecrafts_tags" {
-  description = "List of tags values across all orbital_spacecrafts"
-  value       = [for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : v.tags]
+  description = "Map of tags values across all orbital_spacecrafts, keyed the same as var.orbital_spacecrafts"
+  value       = { for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : k => v.tags }
 }
 output "orbital_spacecrafts_title_line" {
-  description = "List of title_line values across all orbital_spacecrafts"
-  value       = [for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : v.title_line]
+  description = "Map of title_line values across all orbital_spacecrafts, keyed the same as var.orbital_spacecrafts"
+  value       = { for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : k => v.title_line }
 }
 output "orbital_spacecrafts_two_line_elements" {
-  description = "List of two_line_elements values across all orbital_spacecrafts"
-  value       = [for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : v.two_line_elements]
+  description = "Map of two_line_elements values across all orbital_spacecrafts, keyed the same as var.orbital_spacecrafts"
+  value       = { for k, v in azurerm_orbital_spacecraft.orbital_spacecrafts : k => v.two_line_elements }
 }
 
